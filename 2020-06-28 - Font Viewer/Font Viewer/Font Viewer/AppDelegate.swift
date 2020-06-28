01425ccfdc86c572295ce1bab9960fd97404117c
Andrés Mejía <andmej@gmail.com>
2020-06-28 15:48:38 -0700

    Empty app created through XCode.
    Following the tutorial here: https://www.appcoda.com/macos-programming/

---
+++ 2020-06-28 - Font Viewer/Font Viewer/Font Viewer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Font Viewer
+//
+//  Created by Andrés Mejía on 6/28/20.
+//  Copyright © 2020 Andrés Mejía. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ 2020-06-28 - Font Viewer/Font Viewer/Font Viewer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Font Viewer
+//
+//  Created by Andrés Mejía on 6/28/20.
+//  Copyright © 2020 Andrés Mejía. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
